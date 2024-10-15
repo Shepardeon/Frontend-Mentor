d6915baf44aa3dd78e8f7432cfb98f5b8083e193
Shepardeon <thomas.demaen@life-sensation.fr>
2024-10-15 18:28:03 +0200

    simple grid layout

--- product-list-with-cart/src/App.svelte
+++ product-list-with-cart/src/App.svelte
@@ -1,1 +1,26 @@
-<main></main>
+<main class="order">
+  <section class="desserts">
+    <h1>Desserts</h1>
+    <p>TODO -- LIST</p>
+  </section>
+  <section class="cart">
+    <p>TODO -- CART</p>
+  </section>
+</main>
+
+<style>
+  .order {
+    display: grid;
+  }
+
+  .desserts h1 {
+    font-size: var(--fs-900);
+    font-weight: var(--fw-bold);
+  }
+
+  @media (min-width: 600px) {
+    .order {
+      grid-template-columns: 3fr 1fr;
+    }
+  }
+</style>
